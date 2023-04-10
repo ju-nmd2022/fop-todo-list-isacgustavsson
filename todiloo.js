b1a6c5266645c0d1b6e628f7b447f29d2d5e1cf0
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2023-04-10 17:37:23 +0200

    todo 2.0

--- todiloo.js
+++ todiloo.js
@@ -1,28 +1,105 @@
-const toDos = [];
-let toDoList = document.getElementById("toDoList");
-let addButton = document.getElementById("addButton");
-let textBox = document.getElementById("textBox");
-let removeButton = document.createElement("button");
+const list_el = document.getElementById("list");
+const create_btn_el = document.getElementById("create");
 
-addButton.addEventListener("click", itemHandler);
+let todos = [];
 
-function itemHandler() {
-  let newItem = document.createElement("div");
-  let removeButton = document.createElement("button");
+create_btn_el.addEventListener("click", createNewTodo);
 
-  removeButton.addEventListener("click", removeToDoItem);
+function createNewTodo() {
+  const item = {
+    id: new Date().getTime(),
+    text: "",
+    completed: false,
+  };
 
-  newItem.innerText = textBox.value;
-  newItem.classList.add("toDo");
-  toDoList.appendChild(newItem);
-  toDos.push(newItem);
+  todos.unshift(item);
 
-  removeButton.classList.add("toDo");
-  removeButton.setAttribute("id", "removeButton");
-  removeButton.innerText = "remove.";
-  newItem.appendChild(removeButton);
+  const { item_el, input_el } = createTodoElement(item);
+
+  list_el.prepend(item_el);
+
+  input_el.removeAttribute("disabled");
+  input_el.focus();
+
+  save();
 }
 
-function removeToDoItem() {
-  console.log("it works");
+function createTodoElement(item) {
+  const item_el = document.createElement("div");
+  item_el.classList.add("item");
+
+  const input_el = document.createElement("input");
+  input_el.type = "text";
+  input_el.value = item.text;
+  input_el.setAttribute("disabled", "");
+
+  const actions_el = document.createElement("div");
+  actions_el.classList.add("actions");
+
+  const edit_btn_el = document.createElement("button");
+  edit_btn_el.classList.add("edit");
+  edit_btn_el.innerText = "edit";
+
+  const remove_btn_el = document.createElement("button");
+  remove_btn_el.classList.add("remove");
+  remove_btn_el.innerText = "remove";
+
+  actions_el.append(edit_btn_el);
+  actions_el.append(remove_btn_el);
+
+  item_el.append(input_el);
+  item_el.append(actions_el);
+
+  //EVENTS
+
+  input_el.addEventListener("input", () => {
+    item.text = input_el.value;
+  });
+
+  input_el.addEventListener("blur", () => {
+    input_el.setAttribute("disabled", "");
+    save();
+  });
+
+  edit_btn_el.addEventListener("click", () => {
+    input_el.removeAttribute("disabled");
+    input_el.focus();
+  });
+
+  remove_btn_el.addEventListener("click", () => {
+    todos = todos.filter((t) => t.id != item.id);
+
+    item_el.remove();
+
+    save();
+  });
+
+  return { item_el, input_el, edit_btn_el, remove_btn_el };
+}
+
+function displayTodos() {
+  load();
+
+  for (let i = 0; i < todos.length; i++) {
+    const item = todos[i];
+
+    const { item_el } = createTodoElement(item);
+
+    list_el.append(item_el);
+  }
+}
+displayTodos();
+
+function save() {
+  const save = JSON.stringify(todos);
+
+  localStorage.setItem("my_todos", save);
+}
+
+function load() {
+  const data = localStorage.getItem("my_todos");
+
+  if (data) {
+    todos = JSON.parse(data);
+  }
 }
